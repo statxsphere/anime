5fcf4d647e944d7da6b984850ab239a2a6d05088
statxsphere <67361743+statxsphere@users.noreply.github.com>
2021-02-03 18:11:05 +0530

    Add files via upload

--- st_deploy.py
+++ st_deploy.py
@@ -1,23 +1,40 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as pex
+# import numpy as np
+# import scipy as sp
+# from sklearn.metrics.pairwise import cosine_similarity
 
-anime = pd.read_csv("data1/finalanime.csv")
-items = pd.read_csv('data1/items.csv')
 
-def AnimeScout(x,n):
-    anime_name = anime[anime['name'].str.contains(x, case=False)].sort_values(by='members', ascending=False).reset_index()['name'][0]
-    count = 1
-    st.write('If you like {}, you may also like:\n'.format(anime_name))
-    for item in items.sort_values(by = anime_name, ascending = False).name[1:int(n)+1]:
-        st.write('No. {}: {}'.format(count, item))
-        count +=1
+@st.cache 
+def load_animedata():
+    return pd.read_csv("data1/finalanime.csv")
 
-fig = pex.treemap(anime.dropna(how='any'), path=['type','genre1', 'name'], values='members',
-                  color='rating', hover_data=['rating','episodes'],
-                  color_continuous_scale='RdBu')
-fig.update_layout(title_text="Which Anime Should You Watch?",
-                  font_size=10, autosize=False, width=800, height=500, margin=dict(l=20, r=40, t=100, b=20))
+anime = load_animedata()
+maxmem = anime['members'].max()
+
+types = st.sidebar.selectbox('Select your medium:', anime['type'].drop_duplicates())
+genres = st.sidebar.selectbox('Select your genre:', anime['genre1'].drop_duplicates())
+ratings = st.sidebar.slider('Rating:',min_value=0,max_value=10,value=(0,10))
+members = st.sidebar.slider('Members:',min_value=0,max_value=maxmem,value=(0,maxmem),step=1)
+
+@st.cache 
+def set_filters():
+    df = anime.loc[(anime['type']=types) & (anime['genre1']=genres) & 
+                   (anime['rating']= ratings) & (anime['members']=members)]
+    return df
+
+@st.cache(allow_output_mutation=True)
+def figM():
+    anime = set_filters()
+    fig = pex.treemap(anime.dropna(how='any'), path=['type','genre1', 'name'], values='members',
+                      color='rating', hover_data=['rating','episodes'],
+                      color_continuous_scale='RdBu')
+    fig.update_layout(title_text="Which Anime Should You Watch?",
+                      font_size=10, autosize=False, width=800, height=500, margin=dict(l=20, r=40, t=100, b=20))
+    return fig
+
+fig = figM()
 
 st.title("So, what anime should you watch?")
 st.write('''I've come up with two complimentary methods to help you plan your anime journey out.
@@ -49,6 +66,45 @@ st.write('''#### How does the Anime Scout work?
 So, what are you waiting for? Let's scout some anime!''')
 
 x = st.text_input('Tell me an anime you like:')
+n = st.number_input('How many anime should I scout?', min_value=1,max_value=25,value=10,step=1)
+anime_name = anime[anime['name'].str.contains(x, case=False)].sort_values(by='members', ascending=False).reset_index()['name'][0]
+@st.cache 
+def load_itemsdata():
+    # user_sub = pd.read_csv('data1/ratingSub.zip')
+    # user_sub = user_sub.apply(pd.to_numeric,downcast='integer')
+    # merged = user_sub.merge(anime, left_on = 'anime_id', right_on = 'anime_id', suffixes= ['_user', ''])
+    # del user_sub
+    # merged.rename(columns = {'rating_user':'user_rating'}, inplace = True)
+    # ints = merged.select_dtypes(include=['int'])
+    # floats = merged.select_dtypes(include=['float'])
+    # converted_int = ints.apply(pd.to_numeric,downcast='integer')
+    # convMem = merged.members.astype('int32')
+    # converted_flt = floats.apply(pd.to_numeric,downcast='float')
+    # merged[converted_int.columns] = converted_int
+    # merged[converted_flt.columns] = converted_flt
+    # merged['members'] = convMem
+    # del ints,floats,converted_int,convMem,converted_flt
+    # piv = merged.pivot_table(index=['user_id'], columns=['name'], values='user_rating').apply(pd.to_numeric,downcast='float')
+    # del merged
+    # piv_norm = piv.apply(lambda x: (x-np.mean(x))/(np.max(x)-np.min(x)), axis=1)
+    # del piv
+    # piv_norm.fillna(0, inplace=True)
+    # piv_norm = piv_norm.T
+    # piv_norm = piv_norm.loc[:, (piv_norm != 0).any(axis=0)]
+    # piv_sparse = sp.sparse.csr_matrix(piv_norm.values) 
+    # items = pd.DataFrame(cosine_similarity(piv_sparse), index = piv_norm.index, columns = piv_norm.index)
+    # del piv_norm, piv_sparse
+    items = pd.read_csv('https://www.dropbox.com/s/29gp0edhsnr25nu/items.csv?dl=1',usecols=['name',anime_name])
+    return items
+
+def AnimeScout(x,n):
+    items = load_itemsdata()
+    anime_name = anime[anime['name'].str.contains(x, case=False)].sort_values(by='members', ascending=False).reset_index()['name'][0]
+    count = 1
+    st.write('If you like {}, you may also like:\n'.format(anime_name))
+    for item in items.sort_values(by = anime_name, ascending = False).name[1:n+1]:
+        st.write('No. {}: {}'.format(count, item))
+        count +=1
 
 if st.button('Scout Anime!'):
-    AnimeScout(x,10)
+    AnimeScout(x,n)
